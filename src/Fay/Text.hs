c07547ba50b3cc9ed3ee96ba8d931594a60ffec0
Adam Bergmark <adam@edea.se>
2013-07-02 00:35:22 +0200

    Copy implementation from yesod-fay

--- src/Fay/Text.hs
+++ src/Fay/Text.hs
@@ -1,15 +1,39 @@
-{-# Language EmptyDataDecls, NoImplicitPrelude #-}
-
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE CPP                #-}
+-- | Module to be shared between server and client.
+--
+-- This module must be valid for both GHC and Fay.
 module Fay.Text where
 
-import Prelude
-import FFI
-import Data.Data
+import           Prelude
+#ifdef FAY
+import           FFI
+#else
+import           Language.Fay.FFI
+#endif
+import           Data.Data
+
+#ifdef FAY
 
 data Text = Text
+    deriving (Show, Read, Eq, Typeable, Data)
 
 pack :: String -> Text
 pack = ffi "%1"
 
 unpack :: Text -> String
 unpack = ffi "%1"
+
+#else
+
+import qualified Data.Text as T
+
+type Text = T.Text
+
+pack :: String -> Text
+pack = T.pack
+
+unpack :: Text -> String
+unpack = T.unpack
+
+#endif
